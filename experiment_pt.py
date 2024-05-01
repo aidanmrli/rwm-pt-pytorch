38b0faa1d1dcc674cb82b18a51643626446bf5e6
aidanmrli <aidanmrli@gmail.com>
2024-04-30 23:02:20 -0400

    revamped experiment suite

--- experiment_pt.py
+++ experiment_pt.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     dim = 20    # dimension of the target and proposal distributions
     # run many simulations for different variance values
-    var_value_range = np.linspace(0.001, 4, 40)
+    swap_acceptance_rates_range = np.linspace(0.01, 0.8, 20)
     num_seeds = 5
 
     # save results for plotting
@@ -23,20 +23,24 @@ if __name__ == "__main__":
 
     # keep scaling factors consistent in the target density across experiments
     target_density_scaled = MultimodalDensityIID(dim, scaling=True)
+     # temp_beta_ladder = [1, 0.6785718239036314, 0.43023132454234414, 0.2725213895029845, 0.18473698921221351, 0.01]
+    temp_beta_ladder = [1, 0.7040429390962937, 0.4780498066581084, 0.3336747815093883, 0.2331427383329218, 0.16114482945558686, 0.11392056374013135, 0.07950805093109076, 0.05184317948035542, 0.01]
 
-    for var in var_value_range:
-        variance = (var ** 2) / (dim ** (1))
+
+    for a in swap_acceptance_rates_range:
         seed_results_acceptance = []
         seed_results_esjd = []
 
         for seed_val in range(num_seeds):
             simulation = MCMCSimulation(dim=dim, 
-                                        sigma=variance,  # 2.38**2 / dim
-                                        num_iterations=100000,
-                                        algorithm=RandomWalkMH,
-                                        target_dist=target_density_scaled,  # scaling=True for random scaling factors for the components
-                                        symmetric=True,  # whether to do Metropolis or Metropolis-Hastings: symmetric proposal distribution
-                                        seed=seed_val)
+                            sigma=((2.38 ** 2) / (dim ** (1))),  # 2.38**2 / dim
+                            num_iterations=50000,
+                            algorithm=ParallelTemperingRWM,
+                            target_dist=MultimodalDensityNew(dim),  # scaling=True for random scaling factors for the components
+                            symmetric=True,  # whether to do Metropolis or Metropolis-Hastings: symmetric proposal distribution
+                            seed=seed_val,
+                            beta_ladder=None,
+                            swap_acceptance_rate=a)
             
             chain = simulation.generate_samples()
             seed_results_acceptance.append(simulation.acceptance_rate())
@@ -47,25 +51,12 @@ if __name__ == "__main__":
         expected_squared_jump_distances.append(np.mean(seed_results_esjd))
 
     print(f"Maximum ESJD: {max(expected_squared_jump_distances)}")
-    print(f"Acceptance rate corresponding to maximum ESJD: {acceptance_rates[np.argmax(expected_squared_jump_distances)]}")
-    print(f"Variance value corresponding to maximum ESJD: {var_value_range[np.argmax(expected_squared_jump_distances)]}")
+    print(f"Swap acceptance rate corresponding to maximum ESJD: {acceptance_rates[np.argmax(expected_squared_jump_distances)]}")
     # plot results
     plt.plot(acceptance_rates, expected_squared_jump_distances, label='Expected squared jump distance', marker='x')   
     plt.xlabel('acceptance rate')
     plt.ylabel('ESJD')
-    plt.title(f'ESJD vs acceptance rate (dim={dim})')
-    plt.show()
-
-    plt.plot(var_value_range, acceptance_rates, label='Acceptance rate', marker='x')  # marker='o'
-    plt.xlabel('Variance value (value^2 / dim)')
-    plt.ylabel('Acceptance rate')
-    plt.title(f'Acceptance rate for different variance values (dim={dim})')
-    plt.show()
-
-    plt.plot(var_value_range, expected_squared_jump_distances, label='Expected squared jump distance', marker='x')  # marker='o'
-    plt.xlabel('Variance value (value^2 / dim)')
-    plt.ylabel('ESJD')
-    plt.title(f'ESJD for different variance values (dim={dim})')
+    plt.title(f'ESJD vs swap acceptance rate (dim={dim})')
     plt.show()
 
     # see the last histogram to see if results are consistent
