210493f8332f2657a40cf7c5bcab7c712ca6e026
aidanmrli <aidanmrli@gmail.com>
2024-05-24 01:45:00 -0400

    minor update

--- rwm_script.sh
+++ rwm_script.sh
@@ -8,12 +8,12 @@
 
 # python3 experiment_RWM.py --dim 20 --var_max 4.0 --target RoughCarpetScaled --num_iters 100000 --init_seed 0 --num_seeds 5
 # python3 experiment_RWM.py --dim 30 --var_max 4.0 --target RoughCarpetScaled --num_iters 100000 --init_seed 0 --num_seeds 5
-python3 experiment_RWM.py --dim 50 --var_max 4.0 --target RoughCarpetScaled --num_iters 100000 --init_seed 0 --num_seeds 5
+# python3 experiment_RWM.py --dim 50 --var_max 4.0 --target RoughCarpetScaled --num_iters 100000 --init_seed 0 --num_seeds 5
 
-python3 experiment_RWM.py --dim 20 --var_max 4.0 --target ThreeMixture --num_iters 100000 --init_seed 0 --num_seeds 5
-python3 experiment_RWM.py --dim 30 --var_max 4.0 --target ThreeMixture --num_iters 100000 --init_seed 0 --num_seeds 5
+# python3 experiment_RWM.py --dim 20 --var_max 4.0 --target ThreeMixture --num_iters 100000 --init_seed 0 --num_seeds 5
+# python3 experiment_RWM.py --dim 30 --var_max 4.0 --target ThreeMixture --num_iters 100000 --init_seed 0 --num_seeds 5
 python3 experiment_RWM.py --dim 50 --var_max 4.0 --target ThreeMixture --num_iters 100000 --init_seed 0 --num_seeds 5
 
 python3 experiment_RWM.py --dim 20 --var_max 4.0 --target ThreeMixtureScaled --num_iters 100000 --init_seed 0 --num_seeds 5
-python3 experiment_RWM.py --dim 30 --var_max 4.0 --target ThreeMixtureScaled --num_iters 100000 --init_seed 0 --num_seeds 5
+# python3 experiment_RWM.py --dim 30 --var_max 4.0 --target ThreeMixtureScaled --num_iters 100000 --init_seed 0 --num_seeds 5
 python3 experiment_RWM.py --dim 50 --var_max 4.0 --target ThreeMixtureScaled --num_iters 100000 --init_seed 0 --num_seeds 5
