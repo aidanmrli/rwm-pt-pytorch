47fab72ffebe539f3a81763fb50f59bd620d1aa3
aidanmrli <aidan.li@mail.utoronto.ca>
2025-06-08 19:29:29 -0400

    update to params

--- rwm_script.sh
+++ rwm_script.sh
@@ -22,12 +22,12 @@ python3 experiment_RWM.py --dim 50 --var_max 0.9 --target IIDBeta --num_iters 10
 # python3 experiment_RWM.py --dim 50 --var_max 1.2 --target Hypercube --num_iters 100000 --init_seed 0 --num_seeds 5
 # python3 experiment_RWM.py --dim 50 --var_max 1.0 --target Hypercube --num_iters 100000 --init_seed 0 --num_seeds 5
 
-python single_run_experiment.py --target RoughCarpet --dim 2 --normal_base_variance 24.0 --num_iters 100000 --burn_in 10000
-python single_run_experiment.py --target RoughCarpet --dim 5 --normal_base_variance 14.0 --num_iters 100000 --burn_in 10000
-python single_run_experiment.py --target RoughCarpet --dim 10 --normal_base_variance 5.7 --num_iters 100000 --burn_in 10000
-python single_run_experiment.py --target RoughCarpet --dim 20 --normal_base_variance 5.0 --num_iters 100000 --burn_in 10000
-python single_run_experiment.py --target RoughCarpet --dim 30 --normal_base_variance 4.7 --num_iters 100000 --burn_in 10000
-python single_run_experiment.py --target RoughCarpet --dim 50 --normal_base_variance 4.7 --num_iters 100000 --burn_in 10000
+python single_run_experiment.py --target RoughCarpet --dim 2 --normal_base_variance 24.0 --num_iters 100000 --burn_in 1000
+python single_run_experiment.py --target RoughCarpet --dim 5 --normal_base_variance 14.0 --num_iters 100000 --burn_in 1000
+python single_run_experiment.py --target RoughCarpet --dim 10 --normal_base_variance 5.7 --num_iters 100000 --burn_in 1000
+python single_run_experiment.py --target RoughCarpet --dim 20 --normal_base_variance 5.0 --num_iters 100000 --burn_in 1000
+python single_run_experiment.py --target RoughCarpet --dim 30 --normal_base_variance 4.7 --num_iters 100000 --burn_in 1000
+python single_run_experiment.py --target RoughCarpet --dim 50 --normal_base_variance 4.7 --num_iters 100000 --burn_in 1000
 
 # python3 experiment_RWM.py --dim 2 --var_max 4.0 --target ThreeMixture --num_iters 100000 --init_seed 0 --num_seeds 5
 # python3 experiment_RWM.py --dim 5 --var_max 4.0 --target ThreeMixture --num_iters 100000 --init_seed 0 --num_seeds 5
